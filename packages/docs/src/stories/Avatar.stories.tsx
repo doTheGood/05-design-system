71ddcf0e7ce6bcba8dd3236e31da507f3fc6d22e
Diogo Novaes <diogo.dasilva@emea.adecco.net>
2022-10-15 21:08:42 +0200

    storybook adjustments

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/doTheGood.png',
     alt: 'Dino Sauro',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
