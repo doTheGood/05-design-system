14623b467277767dbf5e79af7e40f6a53ec83aa2
Diogo Novaes <diogo.dasilva@emea.adecco.net>
2022-11-27 21:09:00 +0100

    added Tooltip and Toaster components

---
+++ packages/docs/src/stories/Toast.stories.tsx
@@ -0,0 +1,39 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Button, Toast, ToastProps } from '@pal-land/react'
+import { useEffect, useRef, useState } from 'react'
+
+const DemoToast = (props: ToastProps) => {
+  const [isOpen, setOpen] = useState(false)
+
+  const timeRef = useRef(0)
+
+  useEffect(() => {
+    return () => clearTimeout(timeRef.current)
+  }, [])
+
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          setOpen(false)
+          window.clearTimeout(timeRef.current)
+          timeRef.current = window.setTimeout(() => setOpen(true), 100)
+        }}
+      >
+        Agendar
+      </Button>
+      <Toast open={isOpen} onOpenChange={setOpen} {...props} />
+    </div>
+  )
+}
+
+export default {
+  title: 'Form/Toast',
+  component: DemoToast,
+  args: {
+    title: 'Appointment ready',
+    description: 'Friday, 13th November at 16:30',
+  },
+} as Meta<ToastProps>
+
+export const Primary: StoryObj<ToastProps> = {}
